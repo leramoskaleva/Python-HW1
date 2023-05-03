d3419df93b8cf8677215a47226c79b8faae415e6
leramoskaleva <123755849+leramoskaleva@users.noreply.github.com>
2023-05-04 01:34:42 +0300

    Update task28.py

--- HW_5/task28.py
+++ HW_5/task28.py
@@ -3,10 +3,9 @@
 # только +1 и -1. Также нельзя использовать циклы.
 
 def sum(a, b):
-    if a <= 0 or b <= 0:
-        print('Введите положительное число')
-    else: 
-        return ((a + 1) + (b - 1))
+    if b == 0:
+        return a
+    return sum(a + 1, b - 1)
 
 a = int(input())
 b = int(input()) 
