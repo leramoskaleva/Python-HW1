766a5a54fa5852ba7d66531463286b090acfc1b9
leramoskaleva <lerakiparis@gmail.com>
2023-05-03 20:34:58 +0300

    added task 26, 28

---
+++ HW_5/task26.py
@@ -0,0 +1,13 @@
+# Напишите программу, которая на вход принимает два числа A и B,
+# и возводит число А в целую степень B с помощью рекурсии.
+
+def stepen_numbers(A, B):
+    if (B == 1):
+        return A
+    if (B != 1):
+        return (A * stepen_numbers(A, B - 1))
+
+
+A = int(input())
+B = int(input())
+print(stepen_numbers(A, B))
